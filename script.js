c1d1a5b7511c5b20761e5deba6c5cae12270783f
Jack Funck <jackfunck21@gmail.com>
2021-01-12 16:58:49 -0500

    added Url for current and five day

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 
-var apiKey = "d2e0223e1a9e7632ee019305144e0c03";
+var searchBtn = $("#searchBtn");
+
+var apiKey = "f0a0222c2ff140f616209ec3a5abfc21";
+
+var citInput = $("#cityInput").val();
+// The URL
+var currentUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + citInput + "&appid=" + apiKey; 
+
+var fiveDayUrl ="https://api.openweathermap.org/data/2.5/forecast?q=" + citInput + "&appid=" + apiKey; 
+
+console.log (currentUrl);
