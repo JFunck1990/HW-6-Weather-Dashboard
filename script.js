54c24e6de386c122edfdc2214f83831b4c5b1d8b
Jack Funck <jackfunck21@gmail.com>
2021-01-15 07:50:45 -0500

    got they cities entered to append to the aside panel

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+$(document).ready(function(){
 var searchBtn = $("#searchBtn");
 
 var apiKey = "f0a0222c2ff140f616209ec3a5abfc21";
@@ -11,11 +11,21 @@ var apiKey = "f0a0222c2ff140f616209ec3a5abfc21";
 searchBtn.on("click", function(){
 
     var citInput = $("#cityInput").val();
+    var data = JSON.parse(localStorage.getItem("cities")) || [];
+    data.push(citInput);
+    localStorage.setItem("cities", JSON.stringify(data));
+    weatherData(citInput);
+    $("#cityInput").val("");
 
+});
+
+function weatherData(cityName){
+    displayCity();
+    
    // The URL for current weather
-var currentUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + citInput + "&appid=" + apiKey; 
+var currentUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey; 
 // five day forecast
-var fiveDayUrl ="https://api.openweathermap.org/data/2.5/forecast?q=" + citInput + "&appid=" + apiKey; 
+var fiveDayUrl ="https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + apiKey; 
 
 console.log (currentUrl);
 
@@ -36,15 +46,31 @@ $.ajax({
 
 
 
-
 });
-//pulling and listing city name
-for(var i = 0; i < localStorage.length; i++){
-    var cityN = localStorage.getItem(i);
+}
+
+function displayCity(){
+    $("#cityList").empty();
+    var data = JSON.parse(localStorage.getItem("cities")) || [];
+
+    //pulling and listing city name
+for(var i = 0; i < data.length; i++){
+    //var cityN = localStorage.getItem(i);
 
     var cityName = $("#cityList").addClass("cityL");
 
-    cityName.append("<li>" + cityName + "</li>");
+    cityName.append("<button data-city="+data[i]+" class='cityBtn'>" + data[i] + "</button>");
 }
+}
+
+$(document).on("click", ".cityBtn", function(e){
+    e.preventDefault();
+    console.log("Inside cityBtn");
+    var city = $(this).attr("data-city");
+    console.log("City Name: "+city);
+    weatherData(city);
+})
+
+displayCity();
 
 });
